ac69d935dc9c1c452e0e70c4d71e8da9d3c022d1
kyleee-max <serverganti493@gmail.com>
2026-04-11 19:31:42 +0700

    Add files via upload

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,30 @@
+export interface Track {
+  id: string
+  videoId: string
+  title: string
+  artist: string
+  album?: string
+  thumbnail: string
+  duration?: string
+  channelId?: string
+}
 
+export interface Playlist {
+  id: string
+  name: string
+  tracks: Track[]
+  coverGradient?: string
+}
+
+export interface SearchResult {
+  tracks: Track[]
+  nextPageToken?: string
+}
+
+export type Genre =
+  | 'All' | 'Pop' | 'Hip-Hop' | 'R&B' | 'Electronic'
+  | 'Rock' | 'Jazz' | 'K-Pop' | 'Indie' | 'Classical' | 'Lo-Fi'
+
+export type Mood =
+  | 'Late Night' | 'Feel Good' | 'Workout' | 'Focus'
+  | 'Heartbreak' | 'Party' | 'Chill' | 'Hype'
