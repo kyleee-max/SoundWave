4a7cb54d1f49b2627007eb528f06fdd399e8383b
kyleee-max <serverganti493@gmail.com>
2026-04-12 01:25:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'i.ytimg.com' },
-      { protocol: 'https', hostname: 'yt3.ggpht.com' },
-      { protocol: 'https', hostname: 'yt3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-    ],
-  },
-}
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  });
 
-module.exports = nextConfig
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    images: {
+      remotePatterns: [
+        { protocol: 'https', hostname: 'i.ytimg.com' },
+        { protocol: 'https', hostname: 'yt3.ggpht.com' },
+        { protocol: 'https', hostname: 'yt3.googleusercontent.com' },
+        { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      ],
+    },
+  }
+
+  module.exports = withPWA(nextConfig);
+  
